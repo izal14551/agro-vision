fdae6bbd71012b8ef742e8896a5ed6e4f6114662
dinaaalyh <dina.aaliyah@gmail.com>
2024-06-05 00:41:19 +0800

    5 juni 2024

--- app/src/main/java/com/capstone/agrovision/HomeActivity.kt
+++ app/src/main/java/com/capstone/agrovision/HomeActivity.kt
@@ -1,20 +1,37 @@
 package com.capstone.agrovision
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.cardview.widget.CardView
+import com.capstone.agrovision.bookmark.BookmarkActivity
+import com.capstone.agrovision.news.NewsActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val newsCard: CardView = findViewById(R.id.newsCard)
+        val bookmarksCard: CardView = findViewById(R.id.bookmarksCard)
+        val fabCamera: FloatingActionButton = findViewById(R.id.fabCamera) as FloatingActionButton
+
+        newsCard.setOnClickListener {
+            val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+        }
+
+        bookmarksCard.setOnClickListener {
+            val intent = Intent(this, BookmarkActivity::class.java)
+            startActivity(intent)
+        }
+
+        fabCamera.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/capstone/agrovision/SignupActivity.kt
+++ app/src/main/java/com/capstone/agrovision/SignupActivity.kt
@@ -1,6 +1,8 @@
 package com.capstone.agrovision
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,20 @@ import androidx.core.view.WindowInsetsCompat
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnRegister = findViewById<Button>(R.id.btnSignup) // Initialize the login button
+        val btnBackToLogin = findViewById<Button>(R.id.loginTab)
+
+        btnRegister.setOnClickListener {
+            // Logika untuk mendaftarkan pengguna baru
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBackToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
