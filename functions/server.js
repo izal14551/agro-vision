7196ca2b8dd791390efb4e16d9cf01f25a97e95f
izal1455 <erwin14551@gmail.com>
2024-06-08 00:42:30 +0700

    deploy

--- functions/server.js
+++ functions/server.js
@@ -12,8 +12,8 @@ admin.initializeApp({
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
+        port: process.env.PORT || 3000,
+        host: '0.0.0.0'
     });
 
     server.route({
